b259e5e373216923e21ed36b706f442abb51a421
AleIR57 <alejandroriiico@gmail.com>
2022-10-01 12:27:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AlquiNow',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
